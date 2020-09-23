a654df73eb049efa726787996545761f14fc2579
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2020-09-23 18:38:59 +0300

    Added error layout

---
+++ layouts/error.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app app dark>
+    <v-content>
+      <v-container fluid>
+        <h1>Error 404: Page not found</h1>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
